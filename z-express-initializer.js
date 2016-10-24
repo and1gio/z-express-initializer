92d07ee6411ed11ccddd855290302a913dd8e349
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-10-24 11:51:13 +0400

    added featrues for web

--- z-express-initializer.js
+++ z-express-initializer.js
@@ -13,6 +13,16 @@ module.exports = {
         app.express.use(bodyParser.urlencoded({extended: false}));
         app.express.use(cookieParser());
 
+
+        if(app.config.zApp.viewEngine){
+            app.express.set('views', app.folderPath.app.root + app.config.zApp.viewEngine.dir);
+            app.express.set('view engine', app.config.zApp.viewEngine.type);
+        }
+
+        if(app.config.zApp.staticFolder){
+            app.express.use(express.static(app.folderPath.app.root + app.config.zApp.staticFolder));
+        }
+
         next();
     }
 };
