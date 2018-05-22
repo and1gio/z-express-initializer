e864b6933fc1dffbc21829519bb0e07e7b2f2631
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-05-22 19:05:20 +0400

    ext

--- z-express-initializer.js
+++ z-express-initializer.js
@@ -9,17 +9,22 @@ module.exports = {
         app.express = express();
         app.express.set('port', app.config.zApp.port);
 
-        app.express.use(bodyParser.json());
-        app.express.use(bodyParser.urlencoded({extended: false}));
+        if (app.config.zApp.bodyParser) {
+            app.express.use(bodyParser.json(app.config.zApp.bodyParser));
+        } else {
+            app.express.use(bodyParser.json());
+        }
+
+        app.express.use(bodyParser.urlencoded({ extended: false }));
         app.express.use(cookieParser());
 
 
-        if(app.config.zApp.viewEngine){
+        if (app.config.zApp.viewEngine) {
             app.express.set('views', app.folderPath.app.root + app.config.zApp.viewEngine.dir);
             app.express.set('view engine', app.config.zApp.viewEngine.type);
         }
 
-        if(app.config.zApp.staticFolder){
+        if (app.config.zApp.staticFolder) {
             app.express.use(express.static(app.folderPath.app.root + app.config.zApp.staticFolder));
         }
 
